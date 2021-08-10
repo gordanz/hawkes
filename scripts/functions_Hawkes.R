5d8051bed3cc52b779541991803d589902bc2308
Gordan Zitkovic <gordanz@math.utexas.edu>
2021-08-09 21:20:22 -0500

    No_message

--- scripts/functions_Hawkes.R
+++ scripts/functions_Hawkes.R
@@ -6,7 +6,7 @@ simulate <- function(exe_dir) {
   system(command)
 }
 
-get_data <- function(data_dir) {
+get_results <- function(data_dir) {
   p <<- read.table(paste0(data_dir,"par.csv"), header = TRUE, sep=",")
   dN <<- read.table(paste0(data_dir,"N.csv"), header  = FALSE, sep=",")
   ld <<- read.table(paste0(data_dir,"ld.csv"), header = FALSE, sep=",")
@@ -24,23 +24,28 @@ demean_cols <- function(df) {
 }
 
 E <- function(X) {
-  colMeans(as.data.frame(X))
+  matrix(colMeans(as.data.frame(X)), nrow = 1)
 }
 
 
 plot_cols <- function(df) {
-  if (!("data.frame" %in% class(df)) ) {
+  if (!("data.frame" %in% class(df)) ) 
     df = data.frame(y=df)
-  }
-  df_plot= df %>% 
+  
+    df_plot= df %>% 
     add_column(t = seq(from = 0, to = 1, length.out = nrow(df) )) %>%
     pivot_longer(cols=!t, values_to = "y") 
-  
-  
+
   p = ggplot(data=df_plot, aes(x=t, y=y, color=name))+
-    geom_line()
+    geom_line(size = 0.4) + 
+    theme(legend.position = "none",
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank())+
+    scale_color_brewer(palette = "Dark2")
   print(p)
 }
+
+
 
 plot_rows <- function(df){
   plot_cols(t(df))
--- scripts/test_Hawkes.R
+++ scripts/test_Hawkes.R
@@ -1,29 +1,33 @@
+{
 script_dir = "~/.h/scripts/"
 exe_dir = "~/.h/build/"
 data_dir = "~/.h/build/"
-
 source(paste0(script_dir,"functions_Hawkes.R"))
+}
 
 # parameters
-n=1000; alpha = 2; mu_0 = 10; tau_0 = 1; a_0 = 0.5;
+{
+n=100; alpha = 2; mu_0 = 10; tau_0 = 1; a_0 = 0.5;
   a = 1 - a_0/n; mu = mu_0*n; tau = tau_0*n;
-ndiv = 10000; nthr = 1; nsim = 1;
+ndiv = 1000; nthr = 8; nsim = 125;
+}
 
 # call 
 simulate(exe_dir)
 get_results(data_dir)
 
-# # demean trajectories, sample a few and plot
-ld %>%   
-  slice_sample(n=1) %>%
+# sample a few trajectories and plot them
+B %>%   
+  slice_sample(n=10) %>%
   plot_rows
 
 
- 
-# # check if martingale
-E(dM$V8)
-# 
-# # correlation between increments
-# ggplot(data = dN, aes(x=X100, y=X101))+geom_density_2d()
-# cor(dN$X100, dN$X101)
-# hist(B$X1024)
+# code checks
+mean(B[,ndiv])
+sd(B[,ndiv])
+
+# joint distribution of increments of ld
+ggplot(data = dld, aes(x=V500, y=V501))+
+  geom_density_2d()
+cor(dld$V500, dld$V501)
+hist(dld$V500, nclass=50)
