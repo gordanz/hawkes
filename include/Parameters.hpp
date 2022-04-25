23b378dea4ca99204cfa8d8481f73ef86d45b918
Gordan Zitkovic <gordanz@math.utexas.edu>
2022-04-25 18:47:00 -0500

    No_message

--- include/Parameters.hpp
+++ include/Parameters.hpp
@@ -1,22 +1,73 @@
-#ifndef __PARAMETERS_H__
+/**
+ * @file Parameters.hpp
+ * @author Gordan Zitkovic 
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #define __PARAMETERS_H__
 
 #include <cstdio>
 #include <string>
 
+/**
+ * @brief A class that holds all parameters
+ * 
+ */
 class Parameters
 {
 public: // for now
-    double a, alpha, mu, sigma;
-    size_t ndiv, nsim, nthr;
-    std::string outdir;
+    double a; /*!< The expected number of offspring < 1 */
+    double alpha; /*!< A parameter (exponent) of the Pareto kernel */
+    double mu; /*!< Intensity of the ancestor PPP */
+    double sigma; /*!< A parameter (scale) of the Pareto kernel */
+    size_t ndiv; /*!< Number of divisions of [0,1] */
+    size_t nsim; /*!< Number of simulated paths */
+    size_t nthr; /*!< Number of threads (when using parallelization) */
+    std::string outdir; /*!< Name of the output directory */
 
 public:
+    /**
+     * @brief Construct a new Parameters object without initialization
+     * 
+     */
     Parameters();
+    /**
+     * @brief Construct a new Parameters object from command line arg or values
+     * 
+     * If command line arguments are given, Parameters will be filled
+     * from there. The remaining arguments are values to be used for 
+     * parameters not specified on the command line. 
+     * 
+     */
     Parameters(int, char **, double, double, double, double, size_t, size_t, size_t, const char *);
+    /**
+     * @brief A csv-formatted representation of parameters (for use with R)
+     * 
+     * @return std::string 
+     */
     std::string csv();
+    /**
+     * @brief A human-readable string representation of parameters
+     * without spaces. To be used as output file names.
+     * 
+     * @return std::string 
+     */
     std::string str();
+    /**
+     * @brief A human-readable string representation. To be used for debugging.
+     * 
+     * @return std::string 
+     */
     std::string info();
+    /**
+     * @brief Sends a command to the system to concatenate outputs of
+     * different treads (something like `cat outdir/dN* > outdir/dN.csv`)
+     * 
+     */
     void concatenate_outputs();
 };
 
--- include/cvector.hpp
+++ include/cvector.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file cvector.hpp
+ * @author Gordan Zitkovic 
+ * @brief cvector class-template implementation
+ * 
+ * Implementation of  cvector<T> class
+ * 
+ * @version 0.1
+ * @date 2022-04-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __CVECTOR_H__
 #define __CVECTOR_H__
 
@@ -5,17 +18,38 @@
 #include <iterator>
 #include <vector>
 
+/**
+ * @brief A vector class with memory reuse
+ *
+ * cvector<T> is like vector<T> but with the ability to
+ * reuse the allocated memory. It carries its own myend interator and an
+ * additional member reset which virtually clears the vector.
+ *
+ * @tparam T anything that can be used with vector<T>
+ */
 template <class T>
 class cvector
 {
 public:
-    std::vector<T> data;
-    typename std::vector<T>::iterator myend;
+    std::vector<T> data; /*!< holds the data */
+    typename std::vector<T>::iterator myend; /*! iterator to current end of vector */
 
+    /**
+     * @brief Construct a new cvector object
+     * 
+     */
     cvector()
     {
         myend = data.end();
     };
+
+    /**
+     * @brief Reserve memory.
+     * 
+     * Reserve memory for data without filling it with anything.
+     * 
+     * @param expected_size 
+     */
 
     void reserve(size_t expected_size)
     {
@@ -23,15 +57,33 @@ public:
         myend = data.begin();
     };
 
+    /**
+     * @brief Return the iterator to the start
+     * 
+     * @return std::vector<T>::iterator 
+     */
+
     typename std::vector<T>::iterator begin()
     {
         return (data.begin());
     }
 
+    /**
+     * @brief Return the iterator to end (myend)
+     * 
+     * @return std::vector<T>::iterator 
+     */
+
     typename std::vector<T>::iterator end()
     {
         return (myend);
     }
+
+    /**
+     * @brief Push x to the back of the cvector.
+     * 
+     * @param x 
+     */
     void push_back(T x)
     {
         if (myend == data.end())
@@ -46,21 +98,46 @@ public:
         };
     };
 
+    /**
+     * @brief Return the size of (the used portion of) cvector
+     * 
+     * @return size_t 
+     */
     size_t size()
     {
         return (std::distance(data.begin(), myend));
     };
 
+    /**
+     * @brief Empty the vector without deallocating memory
+     * 
+     */
     void reset()
     {
         myend = data.begin();
     };
+
+    /**
+     * @brief Access element at position i
+     * 
+     * @param i position
+     * @return T value
+     */
 
     T operator[](size_t i)
     {
         return (data[i]);
     }
 };
+
+/**
+ * @brief Print the contents of cvector to a stream
+ * 
+ * @tparam T 
+ * @param output 
+ * @param v 
+ * @return std::ostream& 
+ */
 
 template <class T>
 std::ostream &operator<<(std::ostream &output, cvector<T> &v)
