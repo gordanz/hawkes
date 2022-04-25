dac8e3fc7c51e957d4f22b45d495ef988e75a04f
Gordan Zitkovic <gordanz@math.utexas.edu>
2022-04-25 09:36:25 -0500

    No_message

--- scripts/test_Hawkes.R
+++ scripts/test_Hawkes.R
@@ -8,8 +8,8 @@ source(paste0(script_dir,"functions_Hawkes.R"))
 
 # parameters
 {
-n=1000; alpha = 0.55; mu_0 = 10; sigma_0 = 10; a_0 = 0.5;
-ndiv = 1000; nthr = 8; nsim = 15;
+n=3000; alpha = 2; mu_0 = 1; sigma_0 = 1; a_0 = 0.9;
+ndiv = 500; nthr = 8; nsim = 1;
   
     a = 1 - a_0/n; mu = mu_0*n; sigma = sigma_0/n;
 }
@@ -19,17 +19,23 @@ simulate(exe_dir, data_dir)
 get_results(data_dir)
 
 # sample a few trajectories and plot them
-ld %>%
-  slice_sample(n=15) %>%
+dN%>%
+  slice_sample(n=4) %>%
   plot_rows
+
+print(n/(1-a)/mean(N[,ndiv]));
+print(n/(1-a)/sd(N[,ndiv]))
 # save_to_png("dld-initial", width = 1500)
 # save_to_pdf("B-multiple")
 
 # # code checks
-print(mean(B[,ndiv])/sd(B[,ndiv]))
+# print(mean(B[,ndiv])/sd(B[,ndiv]))
  
 # # joint distribution of increments of ld
 # ggplot(data = dld, aes(x=V500, y=V501))+
 #   geom_density_2d()
 # cor(dld$V500, dld$V501)
 # hist(dld$V500, nclass=50)
+N %>%
+  slice_sample(n=15) %>%
+  plot_rows
